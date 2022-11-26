b773c3251b5a22afa26b1ed7964a79b71dafd9db
1DevilishAngel <116287808+1DevilishAngel@users.noreply.github.com>
2022-11-26 13:04:09 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+while true; do
+
+read -p "Do you want to install? (yes/no) " yn
+
+case $yn in
+	yes ) echo ok, we will proceed;
+		sudo zypper ref
+sudo zypper lu
+while true; do
+
+read -p "Do you want to update? (yes/no) " yn
+
+case $yn in
+	yes ) echo ok, we will proceed;
+		sudo zypper up
+		sudo zypper in konsole
+sudo zypper in dolphin
+sudo zypper in MozillaThunderbird
+sudo zypper in gnome-software
+while true; do
+
+read -p "Do you want to install additional software? (yes/no) " yn
+
+case $yn in
+	yes ) echo ok, we will proceed;
+		exit;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo Please answer yes or no!;;
+esac
+
+done
+
+echo doing stuff...
+;;
+	no ) echo exiting...;
+		sudo zypper in konsole
+sudo zypper in dolphin
+sudo zypper in MozillaThunderbird
+sudo zypper in gnome-software
+while true; do
+
+read -p "Do you want to install additional software? (yes/no) " yn
+
+case $yn in
+	yes ) echo ok, we will proceed;
+		exit;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo Please answer yes or no!;;
+esac
+
+done
+
+echo doing stuff...
+;;
+	* ) echo Please answer yes or no!;;
+esac
+
+done
+
+
+sudo zypper in konsole
+sudo zypper in dolphin
+sudo zypper in MozillaThunderbird
+sudo zypper in gnome-software
+;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo Please answer yes or no!;;
+esac
+
+done
+
+echo doing stuff...
+exit
