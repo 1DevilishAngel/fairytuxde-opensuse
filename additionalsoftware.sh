3d47a2f0609338ed80bc9405a3cd543c626caae0
1DevilishAngel <116287808+1DevilishAngel@users.noreply.github.com>
2022-11-26 15:35:38 +0000

    Add files via upload

---
+++ additionalsoftware.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+sudo zypper in gnome-clocks
+sudo zypper in gnome-clocks-lang
+sudo zypper in gome-weather
+sudo zypper in gome-weather-lang
+sudo zypper in gnome-dictionary
+sudo zypper in gnome-dictionary-lang
+sudo zypper in gnome-calendar
+sudo zypper in gnome-calendar-lang
+sudo zypper in gnome-cotacts
+sudo zypper in gnome-cotacts-lang
